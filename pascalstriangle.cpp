a1609feafbcfadbb6efa417837a0857874e58f00
revanth2805r <mailtorevanth28@gmail.com>
2025-08-10 11:40:58 +0530

    Pascal's Triangle

--- pascalstriangle.cpp
+++ pascalstriangle.cpp
@@ -1,12 +1,20 @@
 class Solution {
 public:
+    vector<int> genrow(int row){
+        int ans=1;
+        vector<int> ansrow;
+        amsrow.push_back(1);
+        for(int col=1;col<row;col++){
+            ans=ans*(row-col);
+            ans=ans/col;
+            ansrow.push_back(ans);
+        }
+        return ansrow;
+    }
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>> a(numRows);
         for(int i=0; i<numRows; i++){
-            a[i].assign(i+1, 1);
-            for(int&& j=1; j<=i/2; j++){
-                  a[i][i-j]=a[i][j]=a[i-1][j-1]+a[i-1][j];
-                        
+            a.push_back(genrow(i));                       
             }              
         }
         return a;
